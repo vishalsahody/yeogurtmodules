5228081e264acb70a701a8e5783478d6ce6b4682
Vishal <vishal.sahody@adelphi.digital>
2016-07-26 16:19:27 +1000

    Cleanup

--- breadcrumbs/breadcrumbs.js
+++ breadcrumbs/breadcrumbs.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var $ = require('jquery');
-require('sticky-kit');
+
 // Constructor
 var Breadcrumbs = function() {
-  $(".breadcrumbs").stick_in_parent();
+  //
 };
 
 module.exports = Breadcrumbs;
